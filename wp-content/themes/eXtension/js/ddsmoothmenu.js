bf69e337897b3cdfc6240b609ee3c9b38dd7d399
Ivelin Denev <idengo@gmail.com>
2013-02-12 15:10:04 -0500

    Update of ddsmoothmenu script and adding a second of delay before page menuesare colapsing.

--- wp-content/themes/eXtension/js/ddsmoothmenu.js
+++ wp-content/themes/eXtension/js/ddsmoothmenu.js
@@ -11,20 +11,25 @@
 //** 2) In IE6, shadows are now always disabled
 
 //** July 27th, 09" (v1.31): Fixed bug so shadows can be disabled if desired.
+//** Feb 2nd, 10" (v1.4): Adds ability to specify delay before sub menus appear and disappear, respectively. See showhidedelay variable below
 
+//** Dec 17th, 10" (v1.5): Updated menu shadow to use CSS3 box shadows when the browser is FF3.5+, IE9+, Opera9.5+, or Safari3+/Chrome. Only .js file changed.
+//** July 17th, 11'- Updated to v 1.51: Menu updated to work properly in popular mobile devices such as iPad/iPhone and Android tablets.
 
 var ddsmoothmenu={
 
 //Specify full URL to down and right arrow images (23 is padding-right added to top level LIs with drop downs):
 arrowimages: {down:['downarrowclass', 'http://blogs.extension.org/contentstaff/wp-content/themes/eXtension/images/down.gif', 23], right:['rightarrowclass', 'http://blogs.extension.org/contentstaff/wp-content/themes/eXtension/images/right.gif']},
-
 transition: {overtime:300, outtime:300}, //duration of slide in/ out animation, in milliseconds
-shadow: {enable:true, offsetx:5, offsety:5},
+shadow: {enable:true, offsetx:5, offsety:5}, //enable shadow?
+showhidedelay: {showdelay: 100, hidedelay: 600}, //set delay in milliseconds before sub menus appear and disappear, respectively
 
 ///////Stop configuring beyond here///////////////////////////
 
 detectwebkit: navigator.userAgent.toLowerCase().indexOf("applewebkit")!=-1, //detect WebKit browsers (Safari, Chrome etc)
 detectie6: document.all && !window.XMLHttpRequest,
+css3support: window.msPerformance || (!document.all && document.querySelector), //detect browsers that support CSS3 box shadows (ie9+ or FF3.5+, Safari3+, Chrome etc)
+ismobile:navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i) != null, //boolean check for popular mobile browsers
 
 getajaxmenu:function($, setting){ //function to fetch external page containing the panel DIVs
 	var $menucontainer=$('#'+setting.contentsource[0]) //reference empty div on page that will hold menu
@@ -59,6 +64,7 @@ buildmenu:function($, setting){
 	$headers.each(function(i){ //loop through each LI header
 		var $curobj=$(this).css({zIndex: 100-i}) //reference current LI header
 		var $subul=$(this).find('ul:eq(0)').css({display:'block'})
+		$subul.data('timers', {})
 		this._dimensions={w:this.offsetWidth, h:this.offsetHeight, subulw:$subul.outerWidth(), subulh:$subul.outerHeight()}
 		this.istopheader=$curobj.parents("ul").length==1? true : false //is top level header?
 		$subul.css({top:this.istopheader && setting.orientation!='v'? this._dimensions.h+"px" : 0})
@@ -67,7 +73,7 @@ buildmenu:function($, setting){
 			+'" class="' + (this.istopheader && setting.orientation!='v'? smoothmenu.arrowimages.down[0] : smoothmenu.arrowimages.right[0])
 			+ '" style="border:0;" />'
 		)
-		if (smoothmenu.shadow.enable){
+		if (smoothmenu.shadow.enable && !smoothmenu.css3support){ //if shadows enabled and browser doesn't support CSS3 box shadows
 			this._shadowoffset={x:(this.istopheader?$subul.offset().left+smoothmenu.shadow.offsetx : this._dimensions.w), y:(this.istopheader? $subul.offset().top+smoothmenu.shadow.offsety : $curobj.position().top)} //store this shadow's offsets
 			if (this.istopheader)
 				$parentshadow=$(document.body)
@@ -79,34 +85,50 @@ buildmenu:function($, setting){
 		}
 		$curobj.hover(
 			function(e){
-				var $targetul=$(this).children("ul:eq(0)")
-				this._offsets={left:$(this).offset().left, top:$(this).offset().top}
-				var menuleft=this.istopheader && setting.orientation!='v'? 0 : this._dimensions.w
-				menuleft=(this._offsets.left+menuleft+this._dimensions.subulw>$(window).width())? (this.istopheader && setting.orientation!='v'? -this._dimensions.subulw+this._dimensions.w : -this._dimensions.w) : menuleft //calculate this sub menu's offsets from its parent
-				if ($targetul.queue().length<=1){ //if 1 or less queued animations
-					$targetul.css({left:menuleft+"px", width:this._dimensions.subulw+'px'}).animate({height:'show',opacity:'show'}, ddsmoothmenu.transition.overtime)
-					if (smoothmenu.shadow.enable){
-						var shadowleft=this.istopheader? $targetul.offset().left+ddsmoothmenu.shadow.offsetx : menuleft
-						var shadowtop=this.istopheader?$targetul.offset().top+smoothmenu.shadow.offsety : this._shadowoffset.y
-						if (!this.istopheader && ddsmoothmenu.detectwebkit){ //in WebKit browsers, restore shadow's opacity to full
-							this.$shadow.css({opacity:1})
+				var $targetul=$subul //reference UL to reveal
+				var header=$curobj.get(0) //reference header LI as DOM object
+				clearTimeout($targetul.data('timers').hidetimer)
+				$targetul.data('timers').showtimer=setTimeout(function(){
+					header._offsets={left:$curobj.offset().left, top:$curobj.offset().top}
+					var menuleft=header.istopheader && setting.orientation!='v'? 0 : header._dimensions.w
+					menuleft=(header._offsets.left+menuleft+header._dimensions.subulw>$(window).width())? (header.istopheader && setting.orientation!='v'? -header._dimensions.subulw+header._dimensions.w : -header._dimensions.w) : menuleft //calculate this sub menu's offsets from its parent
+					if ($targetul.queue().length<=1){ //if 1 or less queued animations
+						$targetul.css({left:menuleft+"px", width:header._dimensions.subulw+'px'}).animate({height:'show',opacity:'show'}, ddsmoothmenu.transition.overtime)
+						if (smoothmenu.shadow.enable && !smoothmenu.css3support){
+							var shadowleft=header.istopheader? $targetul.offset().left+ddsmoothmenu.shadow.offsetx : menuleft
+							var shadowtop=header.istopheader?$targetul.offset().top+smoothmenu.shadow.offsety : header._shadowoffset.y
+							if (!header.istopheader && ddsmoothmenu.detectwebkit){ //in WebKit browsers, restore shadow's opacity to full
+								header.$shadow.css({opacity:1})
+							}
+							header.$shadow.css({overflow:'', width:header._dimensions.subulw+'px', left:shadowleft+'px', top:shadowtop+'px'}).animate({height:header._dimensions.subulh+'px'}, ddsmoothmenu.transition.overtime)
 						}
-						this.$shadow.css({overflow:'', width:this._dimensions.subulw+'px', left:shadowleft+'px', top:shadowtop+'px'}).animate({height:this._dimensions.subulh+'px'}, ddsmoothmenu.transition.overtime)
 					}
-				}
+				}, ddsmoothmenu.showhidedelay.showdelay)
 			},
 			function(e){
-				var $targetul=$(this).children("ul:eq(0)")
-				$targetul.animate({height:'hide', opacity:'hide'}, ddsmoothmenu.transition.outtime)
-				if (smoothmenu.shadow.enable){
-					if (ddsmoothmenu.detectwebkit){ //in WebKit browsers, set first child shadow's opacity to 0, as "overflow:hidden" doesn't work in them
-						this.$shadow.children('div:eq(0)').css({opacity:0})
+				var $targetul=$subul
+				var header=$curobj.get(0)
+				clearTimeout($targetul.data('timers').showtimer)
+				$targetul.data('timers').hidetimer=setTimeout(function(){
+					$targetul.animate({height:'hide', opacity:'hide'}, ddsmoothmenu.transition.outtime)
+					if (smoothmenu.shadow.enable && !smoothmenu.css3support){
+						if (ddsmoothmenu.detectwebkit){ //in WebKit browsers, set first child shadow's opacity to 0, as "overflow:hidden" doesn't work in them
+							header.$shadow.children('div:eq(0)').css({opacity:0})
+						}
+						header.$shadow.css({overflow:'hidden'}).animate({height:0}, ddsmoothmenu.transition.outtime)
 					}
-					this.$shadow.css({overflow:'hidden'}).animate({height:0}, ddsmoothmenu.transition.outtime)
-				}
+				}, ddsmoothmenu.showhidedelay.hidedelay)
 			}
 		) //end hover
 	}) //end $headers.each()
+	if (smoothmenu.shadow.enable && smoothmenu.css3support){ //if shadows enabled and browser supports CSS3 shadows
+		var $toplevelul=$('#'+setting.mainmenuid+' ul li ul')
+		var css3shadow=parseInt(smoothmenu.shadow.offsetx)+"px "+parseInt(smoothmenu.shadow.offsety)+"px 5px #aaa" //construct CSS3 box-shadow value
+		var shadowprop=["boxShadow", "MozBoxShadow", "WebkitBoxShadow", "MsBoxShadow"] //possible vendor specific CSS3 shadow properties
+		for (var i=0; i<shadowprop.length; i++){
+			$toplevelul.css(shadowprop[i], css3shadow)
+		}
+	}
 	$mainmenu.find("ul").css({display:'none', visibility:'visible'})
 },
 
@@ -131,6 +153,3 @@ init:function(setting){
 }
 
 } //end ddsmoothmenu variable
-
-//Initialize Menu instance(s):
-

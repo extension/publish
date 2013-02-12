56fe03ada3d65d729642531a13ec5b94c5ac922e
Ivelin Denev <idengo@gmail.com>
2013-02-11 19:43:59 -0500

    Add widget for administrative links without rss links attached to it

--- wp-content/themes/eXtension/functions.php
+++ wp-content/themes/eXtension/functions.php
@@ -131,3 +131,54 @@ function theme_options_validate( $input ) {
 	
 	return $input;
 }
+
+
+register_widget( 'Meta_Compact' );
+
+
+/**
+ * Meta compact widget class
+ *
+ * Displays log in/out, without RSS feed links, etc.
+ *
+ * @since 2.8.0
+ */
+class Meta_Compact extends WP_Widget {
+
+	function __construct() {
+		$widget_ops = array('classname' => 'Meta_Compact', 'description' => __( "Log in/out, admin, without feed and WordPress links") );
+		parent::__construct('meta_compact', __('Meta Compact'), $widget_ops);
+	}
+
+	function widget( $args, $instance ) {
+		extract($args);
+		$title = apply_filters('widget_title', empty($instance['title']) ? __('Meta Compact') : $instance['title'], $instance, $this->id_base);
+
+		echo $before_widget;
+		if ( $title )
+			echo $before_title . $title . $after_title;
+?>
+			<ul>
+			<?php wp_register(); ?>
+			<li><?php wp_loginout(); ?></li>
+			<?php wp_meta(); ?>
+			</ul>
+<?php
+		echo $after_widget;
+	}
+
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+
+		return $instance;
+	}
+
+	function form( $instance ) {
+		$instance = wp_parse_args( (array) $instance, array( 'title' => '' ) );
+		$title = strip_tags($instance['title']);
+?>
+			<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></p>
+<?php
+	}
+}

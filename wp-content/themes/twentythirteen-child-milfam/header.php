957790fedc0cb173f9360c4d33792384e6737b3e
Ivelin Denev <idengo@gmail.com>
2015-02-13 15:32:22 -0500

    change stule of MFLN theme according to their new logo and new colors

--- wp-content/themes/twentythirteen-child-milfam/header.php
+++ wp-content/themes/twentythirteen-child-milfam/header.php
@@ -35,7 +35,7 @@
 		<header id="masthead" class="site-header" role="banner">
 		  <div class="masthead-inner">
 			<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-				<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
+				<h1 class="site-title"><img src="<?php echo bloginfo('stylesheet_directory');?>/images/MFLN_Logo_2C.png" width="100%" /></h1>
 				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 			</a>
 			

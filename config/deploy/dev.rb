297c0d8c42acd097d0e59817ef3117ad66380be6
Jason Adam Young <jayoung@extension.org>
2016-09-28 10:34:08 -0400

    separate vhost and deploy server logic

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -4,6 +4,7 @@ if(branch = ENV['BRANCH'])
 else
   set :branch, 'master'
 end
-set :vhost, 'dev-blogs.aws.extension.org'
-server vhost, :app, :web, :db, :primary => true
+set :vhost, 'dev-blogs.extension.org'
+set :deploy_server, 'dev-blogs.aws.extension.org'
+server deploy_server, :app, :web, :db, :primary => true
 set :port, 22

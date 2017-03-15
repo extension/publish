893c831709b5a40dc733e8c57f11a53867121478
Ben MacNeill <ben.macneill@gmail.com>
2017-03-15 16:42:11 -0400

    update footer meta

--- wp-content/themes/eXtension/footer.php
+++ wp-content/themes/eXtension/footer.php
@@ -8,14 +8,16 @@
 
 <div id="footer">
     <p>eXtension provides objective and research-based information and learning opportunities that help people improve their lives. eXtension is a critical part of the Cooperative Extension System and an educational partner of 75 land-grant universities in the United States.</p>
-    <p class="copyright">&copy; 2012 eXtension. All rights reserved.</p>
-    <ul>
-        <li><a href="http://www.extension.org/main/partners">partners</a></li>
-        <li><a href="http://www.extension.org/main/privacy">privacy</a></li>
-        <li><a href="http://www.extension.org/main/contact_us">contact us</a></li>
-        <li><a href="http://www.extension.org/main/disclaimer">disclaimer</a></li>
-        <li class="last"><a href="http://www.extension.org/main/termsofuse">terms of use</a></li>
-    </ul>
+
+    <div class="extension-meta-footer">
+      <ul>
+        <li><a href="https://extension.org/membership/current/">eXtension Members</a></li>
+        <li><a href="https://extension.org/privacy/">Privacy</a></li>
+        <li><a href="https://extension.org/contact/">Contact Us</a></li>
+        <li><a href="https://extension.org/terms-of-use/">Terms of Use</a></li>
+      </ul>
+      <p>&nbsp;© <?php echo date('Y'); ?> eXtension. All rights reserved.</p>
+    </div>
 </div>
 
 </div>

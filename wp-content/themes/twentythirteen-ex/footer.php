23b140ae8341cf25172ce169b8ca9a9596e1c321
Ben MacNeill <ben.macneill@gmail.com>
2017-03-15 16:35:21 -0400

    create a child theme and add footer meta

---
+++ wp-content/themes/twentythirteen-ex/footer.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+?>
+
+		</div><!-- #main -->
+    <div class="extension-meta-footer">
+          <ul>
+            <li><a href="https://extension.org/membership/current/">eXtension Members</a></li>
+            <li><a href="https://extension.org/privacy/">Privacy</a></li>
+            <li><a href="https://extension.org/contact/">Contact Us</a></li>
+            <li><a href="https://extension.org/terms-of-use/">Terms of Use</a></li>
+          </ul>
+          <p>&nbsp;© <?php echo date('Y'); ?> eXtension. All rights reserved.</p
+        </div>
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<?php get_sidebar( 'main' ); ?>
+
+			<div class="site-info">
+				<?php do_action( 'twentythirteen_credits' ); ?>
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentythirteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentythirteen' ), 'WordPress' ); ?></a>
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
+	</div><!-- #page -->
+
+	<?php wp_footer(); ?>
+</body>
+</html>

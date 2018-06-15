4a5519d144f7849e5473a0c2fbc08becce4abe61
Jason Young <jay@outfielding.net>
2018-06-15 11:12:25 -0400

    remove dev host

--- config/deploy/dev.rb
+++
@@ -1,9 +0,0 @@
-set :deploy_to, "/services/publish/"
-if(branch = ENV['BRANCH'])
-  set :branch, branch
-else
-  set :branch, 'master'
-end
-set :vhost, 'dev-publish.extension.org'
-set :deploy_server, 'dev-publish.awsi.extension.org'
-server deploy_server, :app, :web, :db, :primary => true
